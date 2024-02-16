69bfe809270ee8d0d18c5740c1a7d47940e0bd0e
AIO <69240351+AIO-Develope@users.noreply.github.com>
2024-02-16 20:13:50 +0100

    Renamed LoginView to AccountView

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import UploadView from '../views/UploadView.vue';
-import LoginView from '../views/LoginView.vue';
+import LoginView from '../views/AccountView.vue';
 
 const routes = [
   { path: '/', component: HomeView },
