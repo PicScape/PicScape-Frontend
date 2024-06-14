7f8d7a242a297402db2d1c38a029f968fd3a8330
AIO <69240351+AIO-Develope@users.noreply.github.com>
2024-06-14 23:22:12 +0200

    Update ImageModal.vue

--- src/components/widgets/ImageModal.vue
+++ src/components/widgets/ImageModal.vue
@@ -4,41 +4,45 @@
       <button class="close" @click="closeModal">&times;</button>
 
       <div class="image-modal-details-container">
-        <img :src="imageURL" alt="Modal Image" class="modal-image" />
-        <div class="information-box">
-          <h3 class="text-set-large">{{ modalContent.title }}</h3>
-          <h3 class="text-set-middle-sub">{{ modalContent.imgId }}</h3>
-        </div>
-        <hr />
+     <img :src="imageURL" id="modal-image" />
 
         <div class="information-box">
-          <div style="margin-bottom: 10px;">
-            <h3 class="text-set-middle-bottom">Description:</h3>
-            <p class="img-description">{{ modalContent.description }}</p>
-          </div>
-          <div style="margin-bottom: 10px;">
-            <h3 class="text-set-middle-bottom">Tags:</h3>
-            <div>
-              <span v-for="tag in modalContent.tags" :key="tag" class="img-tag">{{ tag }}</span>
-            </div>
+          <h3 class="img-title text-set-large"></h3>
+          <h3 class="img-id text-set-middle-sub"></h3>
+
+        </div>
+        <hr>
+
+        <div class="information-box">
+          <div>
+            <div class="text-set-middle-bottom">Description:</div>
+            <div class="img-description text-set-middle-bottom-sub">{{ modalContent.description }}</div>
           </div>
 
-          <div style="margin-bottom: 10px;">
-            <h3 class="text-set-middle-bottom">Uploaded by:</h3>
-            <p class="img-author">{{ modalContent.author }}</p>
+          <div>
+            <div class="text-set-middle-bottom" style="margin-bottom: 5px">Tags:</div>
+            <div class="img-tags text-set-middle-bottom-sub"><span  v-for="tag in modalContent.tags" :key="tag" class="img-tag">{{ tag }}</span></div>
           </div>
 
-          <div style="margin-bottom: 10px;">
-            <h3 class="text-set-middle-bottom">Publish Date:</h3>
-            <p class="img-upload-date">{{ modalContent.uploadDate }}</p>
+          <div >
+            <div class="text-set-middle-bottom">Uploaded by:</div>
+            <div class="img-author text-set-middle-bottom-sub">{{ modalContent.author }}</div>
+          </div>
+
+
+          <div>
+            <div class="text-set-middle-bottom">Publish Date:</div>
+            <div class="img-upload-date text-set-middle-bottom-sub">{{ modalContent.uploadedDate }}</div>
           </div>
         </div>
 
-        <div>
-          <button class="action-button" @click="confirmDelete" id="delete-button">Delete</button>
-          <button class="action-button" id="downloadButton">Download</button>
-        </div>
+
       </div>
+
+      <button id="delete-button" @click="confirmDelete">Delete</button>
+
+      <button id="downloadButton">Download</button>
+
     </div>
   </div>
 </template>
@@ -83,8 +87,57 @@
   </script>
   
   <style>
-  /* Global styles for the modal */
-  .modal-overlay {
+
+.modal {
+  display: none;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.4);
+  animation: fadeIn 0.5s ease-in-out;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+
+.modal-content {
+  background-color: #23272a;
+  margin: 5% auto;
+  padding: 20px;
+  border-radius: 3%;
+
+  width: 430px;
+  max-height: 80%;
+  overflow-y: auto;
+
+  animation: slideIn 0.5s ease-in-out;
+}
+
+@keyframes slideIn {
+  from {
+    transform: translateY(-50px);
+    opacity: 0;
+  }
+
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+.modal-overlay {
     position: fixed;
     top: 0;
     left: 0;
@@ -97,23 +150,14 @@
     align-items: center;
     animation: fadeIn 0.5s ease-in-out;
   }
-  
-  .modal-content {
-    background-color: #23272a;
-    margin: 5% auto;
-    padding: 20px;
-    border: 1px solid #888;
-    border-radius: 4%;
-    width: 500px;
-    max-height: 80%;
-    overflow-y: auto;
-    text-align: left;
-    animation: slideIn 0.5s ease-in-out;
-  }
-  
-  .close {
-    position: absolute;
+
+
+
+
+.close {
     color: #757575;
+    float: right;
+
     top: 10px;
     right: 10px;
     background: none;
@@ -127,93 +171,133 @@
     color: #ff0000;
   }
   
-  .modal-image {
-    width: 150px;
-    height: 150px;
-    margin: 15px;
-    margin-bottom: 0;
-    border-radius: 15%;
-    background-color: #101213;
-    box-shadow: 10px 10px #15181a;
-  }
-  
-  .information-box {
-    margin: 15px;
-  }
-  
-  hr {
-    margin: 15px;
-    border: 1px solid #4e4e4e;
-  }
-  
-  .text-set-large {
-    font-size: 30px;
-  }
-  
-  .text-set-middle-sub,
-  .text-set-middle-bottom-sub {
-    font-size: 17px;
-    color: #e7e7e7;
-  }
-  
-  .text-set-middle-bottom {
-    font-size: 17px;
-    color: #919191;
-  }
-  
-  .img-tag {
-    display: inline-block;
-    background-color: #007bff;
-    color: white;
-    padding: 1px 8px;
-    border-radius: 3px;
-    margin-right: 6px;
-    margin-top: 6px;
-  }
-  
-  .action-button {
-    margin: 5px 15px;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    text-align: center;
-    text-decoration: none;
-    transition: 0.2s ease-in-out;
-  }
-  
-  .action-button:nth-child(1) {
-    background-color: #ff0000;
-    color: white;
-  }
-  
-  .action-button:nth-child(2) {
-    background-color: #007bff;
-    color: white;
-  }
-  
-  .action-button:hover {
-    opacity: 0.8;
-  }
-  
-  @keyframes fadeIn {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
-  
-  @keyframes slideIn {
-    from {
-      transform: translateY(-50px);
-      opacity: 0;
-    }
-    to {
-      transform: translateY(0);
-      opacity: 1;
-    }
-  }
-  </style>
+
+
+
+
+
+#delete-button {
+  float: right;
+  margin-right: 15px;
+  margin-bottom: 5px;
+  padding: 10px 20px;
+  background-color: #ff0000;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  transition: 0.2s ease-in-out;
+  font-weight: 700;
+
+}
+
+#delete-button:hover {
+  background-color: #8d0000;
+
+}
+
+#downloadButton {
+  float: left;
+  margin-left: 15px;
+  margin-bottom: 5px;
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  transition: 0.2s ease-in-out;
+  font-weight: 700;
+
+
+}
+
+#downloadButton:hover {
+  background-color: #0056b3;
+}
+
+
+
+
+.image-modal-details-container {
+  text-align: left;
+  padding-bottom: 10px;
+}
+
+#modal-image {
+  width: 150px;
+  height: 150px;
+  margin: 15px;
+  margin-bottom: 0;
+  border-radius: 15%;
+  background-color: #101213;
+  box-shadow: 10px 10px #15181a;
+
+
+}
+
+
+.information-box {
+  margin: 15px;
+
+}
+
+hr {
+  margin-left: 15px;
+  margin-right: 15px;
+  border: 1px solid #4e4e4e;
+}
+
+.text-set-large {
+  font-size: 30px;
+}
+
+.text-set-middle-sub {
+  font-size: 16px;
+  color: #919191;
+}
+
+.text-set-middle-bottom-sub {
+  font-size: 17px;
+  color: #e7e7e7;
+  font-weight: bold;
+  margin-bottom: 20px;
+
+
+}
+
+.text-set-middle-bottom {
+  color: #919191;
+  font-size: 17px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+img-tag {
+  text-decoration: none;
+  color: #fff;
+  background-color: #007bff;
+  padding: 1px 8px;
+  border-radius: 3px;
+  margin-right: 6px;
+  display: inline-block;
+  transition: background-color 0.3s ease;
+  margin-top: 6px;
+}
+
+.img-tag {
+  display: inline-block;
+  background-color: #007bff;
+  color: white;
+  padding: 1px 8px;
+  border-radius: 3px;
+  margin-right: 6px;
+  margin-top: 6px;
+}
+
+</style>
   
