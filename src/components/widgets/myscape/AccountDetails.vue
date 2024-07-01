71721febb12ec98aee785a5373d99472ac40d246
AIO <69240351+AIO-Develope@users.noreply.github.com>
2024-07-01 19:31:11 +0200

    Update AccountDetails.vue

--- src/components/widgets/myscape/AccountDetails.vue
+++ src/components/widgets/myscape/AccountDetails.vue
@@ -8,13 +8,20 @@
     <div class="bottom">
       <div class="card-content">
         <img src="https://api.picscape.xyz/image/view/22970" alt="Profile Picture" class="profile-pic">
-      <div class="details-container">
-        <div class="username">{{ userObj ? userObj.user.username : 'Loading...' }}</div>
-        <div class="userid">{{ userid }}</div>
+        <div class="details-container">
+          <div class="username">{{ userObj ? userObj.user.username : 'Loading...' }}</div>
+          <div class="userid">Joined on 12.02.2023 20:23</div>
+          <div class="roles-container">
+        <div v-for="(role, index) in roles" :key="index" class="role">
+          {{ role }}
+        </div>
       </div>
+        </div>
       </div>
       
+
     </div>
+    
   </div>
 </template>
 
@@ -25,18 +32,32 @@ export default {
   data() {
     return {
       userid: null,
-      userObj: null
+      userObj: null,
+      roles: ["test"],
     };
   },
   async mounted() {
-    this.userid = this.$route.params.userid;
-    if (!this.userid){
-      const user = await axiosService.checkTokenValidity()
-      this.userid = user.user.id
+    try {
+      
+      this.userid = this.$route.params.userid;
+      if (!this.userid) {
+        const user = await axiosService.checkTokenValidity()
+        if (user) {
+          this.userid = user.user.id;
+          this.roles = user.user.roles || [];
+        } else {
+          window.location.href = '/login';
+        }
+
+      }
+    } catch {
+      console.log("error while mount")
     }
+
 
     try {
       const response = await axiosService.getUser("userId", this.userid);
+      this.roles = response.user.roles || [];
       console.log(response);
       this.userObj = response;
     } catch (error) {
@@ -71,13 +92,15 @@ export default {
   align-items: center;
   width: 100%;
 }
-.card-content{
+
+.card-content {
   display: flex;
   align-items: center;
   position: relative;
   bottom: 40px;
   left: 5%;
 }
+
 .profile-pic {
   width: 100px;
   height: 100px;
@@ -93,6 +116,19 @@ export default {
   padding-bottom: 3px;
 }
 
+.role {
+  background-color: #d3d3d3;
+  border-radius: 4px;
+  padding: 2px 4px;
+  margin-right: 8px;
+  margin-bottom: 8px;
+  display: inline-block;
+  margin-bottom: 0;
+  color: black;
+  font-size: 15px;
+}
+
+
 .userid {
   font-size: 13px;
   font-weight: bold;
@@ -105,4 +141,11 @@ export default {
   font-weight: bold;
   color: var(--white-surface-100);
 }
+
+.roles-container {
+  margin-top: 6px;
+  display: flex;
+  flex-wrap: wrap;
+}
+
 </style>
