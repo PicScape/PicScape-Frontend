ed343e4f89df2477adbc1251b73125a8d6903ce3
AIO <69240351+AIO-Develope@users.noreply.github.com>
2024-07-13 15:00:04 +0200

    Update UploadModal.vue
    modal wasnt on top, edited z-index

--- src/components/widgets/modals/UploadModal.vue
+++ src/components/widgets/modals/UploadModal.vue
@@ -482,7 +482,7 @@ export default {
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
-    z-index: 999;
+    z-index: 2000;
     display: flex;
     justify-content: center;
     align-items: flex-start;
